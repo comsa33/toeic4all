435fc175ab8757704e5f484ecac5bc99b565b91c
ruo <ruo@gravylab.co.kr>
2023-05-21 17:42:19 +0900

    fix: layout.html, mock_test.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,40 +1,42 @@
-document.getElementById('generate-btn').onclick = function() {
-    var difficulty = document.getElementById('difficulty').value;
-    if (!difficulty) {
-        alert('난이도를 선택해주세요.');
-        return;
-    }
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/api/test?TestLv=' + difficulty);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-
-            console.log(xhr.responseText);
-
-            var data = JSON.parse(xhr.responseText).data;
-            var questionHtml = data.questions;
-            var answerHtml = data.answers;
-            var explanationHtml = data.explanations;
-
-            var testResultDiv = document.getElementById('test-result');
-            testResultDiv.innerHTML = '<h2>' + difficulty + ' 파트5 모의고사</h2>' + questionHtml;
-
-            var downloadQuestionBtn = document.getElementById('download-question-btn');
-            downloadQuestionBtn.style.display = 'block';
-            downloadQuestionBtn.onclick = function() {
-                downloadHtml(questionHtml, '문제집.html');
-            };
-
-            var downloadAnswerBtn = document.getElementById('download-answer-btn');
-            downloadAnswerBtn.style.display = 'block';
-            downloadAnswerBtn.onclick = function() {
-                downloadHtml(answerHtml + explanationHtml, '해설지.html');
-            };
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('generate-btn').onclick = function() {
+        var difficulty = document.getElementById('difficulty').value;
+        if (!difficulty) {
+            alert('난이도를 선택해주세요.');
+            return;
         }
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/api/test?TestLv=' + difficulty);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+
+                console.log(xhr.responseText);
+
+                var data = JSON.parse(xhr.responseText).data;
+                var questionHtml = data.questions;
+                var answerHtml = data.answers;
+                var explanationHtml = data.explanations;
+
+                var testResultDiv = document.getElementById('test-result');
+                testResultDiv.innerHTML = '<h2>' + difficulty + ' 파트5 모의고사</h2>' + questionHtml;
+
+                var downloadQuestionBtn = document.getElementById('download-question-btn');
+                downloadQuestionBtn.style.display = 'block';
+                downloadQuestionBtn.onclick = function() {
+                    downloadHtml(questionHtml, '문제집.html');
+                };
+
+                var downloadAnswerBtn = document.getElementById('download-answer-btn');
+                downloadAnswerBtn.style.display = 'block';
+                downloadAnswerBtn.onclick = function() {
+                    downloadHtml(answerHtml + explanationHtml, '해설지.html');
+                };
+            }
+        };
+        xhr.send();
     };
-    xhr.send();
-};
+});
 
 function downloadHtml(html, filename) {
     var element = document.createElement('a');
