73841a6850874a0bd51a2d94a07cd23bee171f57
ruo <ruo@gravylab.co.kr>
2023-05-23 15:14:19 +0900

    fix: script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,41 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('generate-btn').onclick = function() {
-        var difficulty = document.getElementById('difficulty').value;
-        if (!difficulty) {
-            alert('난이도를 선택해주세요.');
-            return;
-        }
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', '/api/test?TestLv=' + difficulty);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-
-                console.log(xhr.responseText);
-
-                var data = JSON.parse(xhr.responseText).data;
-                var questionHtml = data.questions;
-                var answerHtml = data.answers;
-                var explanationHtml = data.explanations;
-
-                var testResultDiv = document.getElementById('test-result');
-                testResultDiv.innerHTML = '<h2>' + 'AI 생성 결과</h2>' + questionHtml;
-
-                var downloadQuestionBtn = document.getElementById('download-question-btn');
-                downloadQuestionBtn.style.display = 'block';
-                downloadQuestionBtn.onclick = function() {
-                    downloadHtml(questionHtml, '문제집.html');
-                };
-
-                var downloadAnswerBtn = document.getElementById('download-answer-btn');
-                downloadAnswerBtn.style.display = 'block';
-                downloadAnswerBtn.onclick = function() {
-                    downloadHtml(answerHtml + explanationHtml, '해설지.html');
-                };
+    var generateBtn = document.getElementById('generate-btn');
+    if (generateBtn) {
+        generateBtn.onclick = function() {
+            var difficulty = document.getElementById('difficulty').value;
+            if (!difficulty) {
+                alert('난이도를 선택해주세요.');
+                return;
             }
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', '/api/test?TestLv=' + difficulty);
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+
+                    console.log(xhr.responseText);
+
+                    var data = JSON.parse(xhr.responseText).data;
+                    var questionHtml = data.questions;
+                    var answerHtml = data.answers;
+                    var explanationHtml = data.explanations;
+
+                    var testResultDiv = document.getElementById('test-result');
+                    testResultDiv.innerHTML = '<h2>' + 'AI 생성 결과</h2>' + questionHtml;
+
+                    var downloadQuestionBtn = document.getElementById('download-question-btn');
+                    downloadQuestionBtn.style.display = 'block';
+                    downloadQuestionBtn.onclick = function() {
+                        downloadHtml(questionHtml, '문제집.html');
+                    };
+
+                    var downloadAnswerBtn = document.getElementById('download-answer-btn');
+                    downloadAnswerBtn.style.display = 'block';
+                    downloadAnswerBtn.onclick = function() {
+                        downloadHtml(answerHtml + explanationHtml, '해설지.html');
+                    };
+                }
+            };
+            xhr.send();
         };
-        xhr.send();
-    };
+    }
 });
 
 function downloadHtml(html, filename) {
@@ -48,20 +51,29 @@ function downloadHtml(html, filename) {
     document.body.removeChild(element);
 }
 
-$("#download-questions").click(function() {
-    var test_no = $("#test-no").text();
-    var url = "/api/download/" + test_no + "/questions";
-    window.open(url, "_blank");
-});
+var downloadQuestions = document.getElementById('download-questions');
+if (downloadQuestions) {
+    downloadQuestions.onclick = function() {
+        var test_no = document.getElementById('test-no').innerText;
+        var url = "/api/download/" + test_no + "/questions";
+        window.open(url, "_blank");
+    };
+}
 
-$("#download-answers").click(function() {
-    var test_no = $("#test-no").text();
-    var url = "/api/download/" + test_no + "/answers";
-    window.open(url, "_blank");
-});
+var downloadAnswers = document.getElementById('download-answers');
+if (downloadAnswers) {
+    downloadAnswers.onclick = function() {
+        var test_no = document.getElementById('test-no').innerText;
+        var url = "/api/download/" + test_no + "/answers";
+        window.open(url, "_blank");
+    };
+}
 
-$("#download-explanations").click(function() {
-    var test_no = $("#test-no").text();
-    var url = "/api/download/" + test_no + "/explanations";
-    window.open(url, "_blank");
-});
+var downloadExplanations = document.getElementById('download-explanations');
+if (downloadExplanations) {
+    downloadExplanations.onclick = function() {
+        var test_no = document.getElementById('test-no').innerText;
+        var url = "/api/download/" + test_no + "/explanations";
+        window.open(url, "_blank");
+    };
+}
