f17d1229cce345f92ca66a2ed174b959fd809993
ruo <ruo@gravylab.co.kr>
2023-06-13 21:56:30 +0900

    fix: script js

--- static/js/script.js
+++ static/js/script.js
@@ -99,9 +99,27 @@ function displayTestResult(data) {
     var testResultDiv = document.getElementById('test-result');
     testResultDiv.innerHTML = '<h2>' + 'AI 생성 결과</h2>' + data.questions;
 
+    // 해설지에 대한 div를 추가
+    testResultDiv.innerHTML += `
+        <button id="answer-toggle">정답 및 해설 보기</button>
+        <div id="answer" style="display:none;">
+            ${data.answers}
+        </div>`;
+
+    // 클릭 이벤트 리스너를 버튼에 추가
+    document.getElementById('answer-toggle').addEventListener('click', function() {
+        var answerDiv = document.getElementById('answer');
+        if (answerDiv.style.display === 'none') {
+            answerDiv.style.display = 'block';
+        } else {
+            answerDiv.style.display = 'none';
+        }
+    });
+
     setupHtmlDownloadButton('download-question-btn', data.questions, '문제집.html');
     setupHtmlDownloadButton('download-answer-btn', data.answers + data.explanations, '해설지.html');
 }
+
 
 function setupHtmlDownloadButton(buttonId, html, filename) {
     var button = document.getElementById(buttonId);
