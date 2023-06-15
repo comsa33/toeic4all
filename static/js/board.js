6712eba62dbf9ba4dd82046c91541a510ce5eca8
ruo <ruo@gravylab.co.kr>
2023-06-15 11:06:34 +0900

    fix: board css html js

--- static/js/board.js
+++ static/js/board.js
@@ -438,11 +438,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.getElementById('toggle-guidelines-button').addEventListener('click', function() {
     var content = document.getElementById('community-guidelines-content');
-    if (content.style.display === 'none') {
-        content.style.display = 'block';
-        this.textContent = '가이드라인 숨기기';
-    } else {
-        content.style.display = 'none';
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
         this.textContent = '가이드라인 보기';
-    }
+    } else {
+        // note: you might need to adjust this value depending on the actual height of your content
+        content.style.maxHeight = content.scrollHeight + "px";
+        this.textContent = '가이드라인 숨기기';
+    } 
 });
