62d7e527ddd6bd89a535ad063781ae086155405c
ruo <ruo@gravylab.co.kr>
2023-06-20 15:47:26 +0900

    fix: user-detail js

--- static/js/user_detail.js
+++ static/js/user_detail.js
@@ -1,26 +1,55 @@
-$('#edit-button').on('click', function() {
-    $('#edit-form').toggle();
-});
+function makeRequest(method, url, headers = {}, callback, errorCallback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    Object.keys(headers).forEach(function(key) {
+        xhr.setRequestHeader(key, headers[key]);
+    });
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            callback(JSON.parse(xhr.responseText));
+        } else if (errorCallback) {
+            errorCallback();
+        }
+    };
+    xhr.send();
+}
 
-$('#toeic-experience').on('change', function() {
-    if (this.value === 'yes') {
-        $('#toeic-score').prop('disabled', false);
-    } else {
-        $('#toeic-score').prop('disabled', true);
-    }
-});
+function updateUI(data) {
+    $('.container p').first().text('사용자 이름: ' + data.username);
+    $('.container p').eq(1).text('가입 날짜: ' + data.registered_on);
+}
 
 $(document).ready(function() {
-    var username = $('#nav-user a').first().text().replace(' 님', ''); // 사용자 이름에서 ' 님'을 제거합니다.
-
-    $.ajax({
-        url: '/user/' + username,
-        method: 'GET',
-        success: function(response) {
-            if (response.success) {
-                $('.container p').first().text('사용자 이름: ' + response.username);
-                $('.container p').eq(1).text('가입 날짜: ' + response.registered_on);
+    var token = localStorage.getItem('access_token');
+    if (token) {
+        makeRequest(
+            'GET',
+            'https://toeic4all.com/user/status',
+            { 'Authorization': `Bearer ${token}` },
+            function(data) {
+                console.log(data);
+                if (data.status == 'logged_in') {
+                    console.log('Logged in');
+                    updateUI(data);
+                }
+            },
+            function() {
+                console.log('Not logged in');
             }
+        );
+    } else {
+        console.log('Not logged in');
+    }
+
+    $('#edit-button').on('click', function() {
+        $('#edit-form').toggle();
+    });
+
+    $('#toeic-experience').on('change', function() {
+        if (this.value === 'yes') {
+            $('#toeic-score').prop('disabled', false);
+        } else {
+            $('#toeic-score').prop('disabled', true);
         }
     });
 });
