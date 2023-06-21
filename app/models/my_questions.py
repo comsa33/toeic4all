6c2ee90a35a08188984fcb0d7898d0c2ae218d06
ruo <ruo@gravylab.co.kr>
2023-06-21 10:24:50 +0900

    fix: my_questions model

--- app/models/my_questions.py
+++ app/models/my_questions.py
@@ -5,5 +5,5 @@ class MyQuestions(db.Model):
     __tablename__ = 'my_questions'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
+    username = db.Column(db.String, nullable=False)
     question_id = db.Column(db.Integer, db.ForeignKey('generated_questions.id'))
