1ba18002610e76df6142d53af5c2b10a6f0d8d5a
ruo <ruo@gravylab.co.kr>
2023-06-22 08:56:07 +0900

    fix: questions js css

--- static/js/questions.js
+++ static/js/questions.js
@@ -7,6 +7,44 @@ function fetchWithToken(url, options = {}) {
     return fetch(url, {...options, headers});
 }
 
+var timerContainer = document.getElementById('timer-container');
+var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+timerContainer.onmousedown = dragMouseDown;
+
+function dragMouseDown(e) {
+    e.preventDefault();
+
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    document.onmouseup = closeDragElement;
+
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+}
+
+function elementDrag(e) {
+    e.preventDefault();
+
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    // set the element's new position:
+    timerContainer.style.top = (timerContainer.offsetTop - pos2) + "px";
+    timerContainer.style.right = (window.innerWidth - timerContainer.offsetLeft - timerContainer.offsetWidth - pos1) + "px";
+}
+
+function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+}
+
 var timer;
 var minutes = 0;
 var seconds = 0;
