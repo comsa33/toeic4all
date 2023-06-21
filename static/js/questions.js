877682629e95221ca2256be25a1cd1af0d9cb2c1
ruo <ruo@gravylab.co.kr>
2023-06-21 22:45:44 +0900

    fix: questions js html

--- static/js/questions.js
+++ static/js/questions.js
@@ -7,91 +7,6 @@ function fetchWithToken(url, options = {}) {
     return fetch(url, {...options, headers});
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('submit-answers').addEventListener('click', function() {
-        var userAnswers = {};
-        var question_numbers = {};
-        var unansweredQuestions = 0;
-
-        Array.from(document.getElementsByClassName('question-container')).forEach(function(container) {
-            var question_id = container.getElementsByClassName('question-id')[0].textContent;
-            var question_number = container.getElementsByClassName('question-number')[0].textContent;
-            var checkedInput = container.querySelector('input[type="radio"]:checked');
-
-            userAnswers[question_id] = checkedInput ? checkedInput.value : '미응답';
-            question_numbers[question_id] = question_number;
-
-            if (!checkedInput) {
-                unansweredQuestions++;
-            }
-        });
-
-        if (unansweredQuestions > 0) {
-            if (confirm('아직 모든 문제를 다 풀지 않았습니다. 정말 채점하고 정답을 보겠습니까? 틀린 문제는 "내 오답노트"에서 확인하실 수 있습니다.')) {
-                scoreTest(userAnswers, question_numbers);
-            }
-        } else {
-            scoreTest(userAnswers, question_numbers);
-        }
-    });
-});
-
-function scoreTest(userAnswers, question_numbers) {
-    var question_ids = Object.keys(userAnswers);
-    question_ids.sort((a, b) => parseInt(question_numbers[a]) - parseInt(question_numbers[b]));
-
-    fetch('/api/answer?QuestionIds=' + question_ids.join(','), {
-        method: 'GET'
-    })
-    .then(response => response.json())
-    .then(data => {
-        var correct_answers = {};
-        data['data'].forEach(answer => {
-            correct_answers[answer['QuestionId']] = answer['AnswerText'][0]; // assuming each question has only one correct answer
-        });
-        
-        var score = 0;
-        var tableBody = document.getElementById('answer-table-body');
-        tableBody.innerHTML = '';
-        
-        for (var i = 0; i < question_ids.length; i++) {
-            var question_id = question_ids[i];
-            
-            var row = document.createElement('tr');
-            var numberCell = document.createElement('td');
-            numberCell.innerText = question_numbers[question_id];
-            row.appendChild(numberCell);
-            var yourAnswerCell = document.createElement('td');
-            yourAnswerCell.innerText = userAnswers[question_id];
-            row.appendChild(yourAnswerCell);
-            var correctAnswerCell = document.createElement('td');
-            correctAnswerCell.innerText = correct_answers[question_id] || '정답 정보 없음';
-            row.appendChild(correctAnswerCell);
-            
-            if (userAnswers[question_id] === correct_answers[question_id]) {
-                score += 1;
-            } else {
-                row.style.backgroundColor = '#FF9494';
-            }
-            
-            tableBody.appendChild(row);
-        }
-        
-        document.getElementById('score').innerText = '점수: ' + score + '/' + question_ids.length;
-        document.getElementById('answer-table').style.display = 'table';
-        
-        // '해설지 보기' 버튼 보이기 및 링크 연결
-        var showExplanationsButton = document.getElementById('show-explanations');
-        showExplanationsButton.style.display = 'block';
-        
-        // 현재 URL에서 ID 부분만 뽑아와서 새 URL 생성
-        var url = new URL(window.location.href);
-        var segments = url.pathname.split('/');
-        var id = segments[segments.length - 1];
-        showExplanationsButton.href = 'https://toeic4all.com/api/test/explanations/' + id;
-    });
-}
-
 var timer;
 var minutes = 0;
 var seconds = 0;
@@ -178,6 +93,114 @@ document.getElementById('toggle-timer-btn').addEventListener('click', function()
         toggleIcon.className = 'fas fa-eye';
     }
 });
+
+document.getElementById('start-test-btn').addEventListener('click', function() {
+    startTest();
+    startTimer();
+    this.style.display = 'none';
+    document.getElementById('end-test-btn').style.display = 'block';
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('submit-answers').addEventListener('click', function() {
+        var userAnswers = {};
+        var question_numbers = {};
+        var unansweredQuestions = 0;
+
+        Array.from(document.getElementsByClassName('question-container')).forEach(function(container) {
+            var question_id = container.getElementsByClassName('question-id')[0].textContent;
+            var question_number = container.getElementsByClassName('question-number')[0].textContent;
+            var checkedInput = container.querySelector('input[type="radio"]:checked');
+
+            userAnswers[question_id] = checkedInput ? checkedInput.value : '미응답';
+            question_numbers[question_id] = question_number;
+
+            if (!checkedInput) {
+                unansweredQuestions++;
+            }
+        });
+
+        if (unansweredQuestions > 0) {
+            if (confirm('아직 모든 문제를 다 풀지 않았습니다. 정말 채점하고 정답을 보겠습니까? 틀린 문제는 "내 오답노트"에서 확인하실 수 있습니다.')) {
+                scoreTest(userAnswers, question_numbers);
+            }
+        } else {
+            scoreTest(userAnswers, question_numbers);
+        }
+    });
+});
+
+var testStartTime;
+
+function startTest() {
+    testStartTime = new Date();
+}
+
+function scoreTest(userAnswers, question_numbers) {
+    var question_ids = Object.keys(userAnswers);
+    question_ids.sort((a, b) => parseInt(question_numbers[a]) - parseInt(question_numbers[b]));
+
+    fetch('/api/answer?QuestionIds=' + question_ids.join(','), {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => {
+        var correct_answers = {};
+        data['data'].forEach(answer => {
+            correct_answers[answer['QuestionId']] = answer['AnswerText'][0]; // assuming each question has only one correct answer
+        });
+
+        var score = 0;
+        var tableBody = document.getElementById('answer-table-body');
+        tableBody.innerHTML = '';
+
+        for (var i = 0; i < question_ids.length; i++) {
+            var question_id = question_ids[i];
+
+            var row = document.createElement('tr');
+            var numberCell = document.createElement('td');
+            numberCell.innerText = question_numbers[question_id];
+            row.appendChild(numberCell);
+            var yourAnswerCell = document.createElement('td');
+            yourAnswerCell.innerText = userAnswers[question_id];
+            row.appendChild(yourAnswerCell);
+            var correctAnswerCell = document.createElement('td');
+            correctAnswerCell.innerText = correct_answers[question_id] || '정답 정보 없음';
+            row.appendChild(correctAnswerCell);
+
+            if (userAnswers[question_id] === correct_answers[question_id]) {
+                score += 1;
+            } else {
+                row.style.backgroundColor = '#FF9494';
+            }
+
+            tableBody.appendChild(row);
+        }
+
+        document.getElementById('score').innerText = '점수: ' + score + '/' + question_ids.length;
+        if (testStartTime) {
+            var testDuration = new Date() - testStartTime;
+            var seconds = Math.round(testDuration / 1000);
+            var minutes = Math.floor(seconds / 60);
+            seconds = seconds % 60;
+            document.getElementById('duration').innerText = '소요시간: ' + minutes + '분 ' + seconds + '초';
+        } else {
+            document.getElementById('duration').innerText = '타이머를 사용하지 않았습니다.';
+        }
+
+        document.getElementById('answer-table').style.display = 'table';
+
+        // '해설지 보기' 버튼 보이기 및 링크 연결
+        var showExplanationsButton = document.getElementById('show-explanations');
+        showExplanationsButton.style.display = 'block';
+
+        // 현재 URL에서 ID 부분만 뽑아와서 새 URL 생성
+        var url = new URL(window.location.href);
+        var segments = url.pathname.split('/');
+        var id = segments[segments.length - 1];
+        showExplanationsButton.href = 'https://toeic4all.com/api/test/explanations/' + id;
+    });
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     Array.from(document.getElementsByClassName('report-btn')).forEach(function(button) {
