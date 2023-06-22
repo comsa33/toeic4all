040726ce708b2b3da77ca51957004f3dbfa48de8
ruo <ruo@gravylab.co.kr>
2023-06-22 14:05:54 +0900

    fix: user-test-detail model

--- app/models/user_test_detail.py
+++ app/models/user_test_detail.py
@@ -12,3 +12,15 @@ class UserTestDetail(db.Model):
     wrong_questions = db.Column(db.Integer)
     duration = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id': self.id,
+            'username': self.username,
+            'test_no': self.test_no,
+            'wrong_questions': self.wrong_questions,
+            'duration': self.duration,
+            'created_at': self.created_at.isoformat() if self.created_at else None,
+        }
