fc3aab33c7a08602fb2e3c33bcd7b15ee559b5cc
ruo <ruo@gravylab.co.kr>
2023-06-26 14:23:01 +0900

    fix: my_learning_analysis js

--- static/js/my_learning_analysis.js
+++ static/js/my_learning_analysis.js
@@ -30,36 +30,58 @@ function fetchWithToken(url, options = {}) {
     }
 }
 
-function displayData(elementId, data) {
+function displayDataInTable(elementId, data) {
     const element = document.getElementById(elementId);
     element.innerHTML = '';  // Clear the existing data
 
-    // Add new data (just as a simple text for now)
-    data.results.forEach(function(result) {
-        const resultText = JSON.stringify(result);
-        const p = document.createElement('p');
-        p.textContent = resultText;
-        element.appendChild(p);
+    // Create a table
+    const table = document.createElement('table');
+
+    // Add header row
+    const thead = document.createElement('thead');
+    const headerRow = document.createElement('tr');
+    Object.keys(data.results[0]).forEach(function(key) {
+        const th = document.createElement('th');
+        th.textContent = key;
+        headerRow.appendChild(th);
     });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    // Add data rows
+    const tbody = document.createElement('tbody');
+    data.results.forEach(function(result) {
+        const row = document.createElement('tr');
+        Object.values(result).forEach(function(value) {
+            const td = document.createElement('td');
+            td.textContent = value;
+            row.appendChild(td);
+        });
+        tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
+
+    // Add the table to the element
+    element.appendChild(table);
 }
 
-// Call the APIs and display the data
+// Call the APIs and display the data in tables
 fetchWithToken('/api/performance/question-type')
     .then(response => response.json())
-    .then(data => displayData('accuracy-by-type', data))
+    .then(data => displayDataInTable('accuracy-by-type', data))
     .catch(err => console.error(err));
 
 fetchWithToken('/api/performance/question-level')
     .then(response => response.json())
-    .then(data => displayData('weak-areas', data))
+    .then(data => displayDataInTable('weak-areas', data))
     .catch(err => console.error(err));
 
 fetchWithToken('/api/performance/time-spent')
     .then(response => response.json())
-    .then(data => displayData('accuracy-time-by-test', data))
+    .then(data => displayDataInTable('accuracy-time-by-test', data))
     .catch(err => console.error(err));
 
 fetchWithToken('/api/growth')
     .then(response => response.json())
-    .then(data => displayData('progress-by-test', data))
+    .then(data => displayDataInTable('progress-by-test', data))
     .catch(err => console.error(err));
