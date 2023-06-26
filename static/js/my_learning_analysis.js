7792e890d0d848fb3bb3cb5f87658b31ec757ce7
ruo <ruo@gravylab.co.kr>
2023-06-26 14:29:53 +0900

    fix: my_learning_analysis js html

--- static/js/my_learning_analysis.js
+++ static/js/my_learning_analysis.js
@@ -30,58 +30,72 @@ function fetchWithToken(url, options = {}) {
     }
 }
 
-function displayDataInTable(elementId, data) {
-    const element = document.getElementById(elementId);
-    element.innerHTML = '';  // Clear the existing data
-
-    // Create a table
-    const table = document.createElement('table');
-
-    // Add header row
-    const thead = document.createElement('thead');
-    const headerRow = document.createElement('tr');
-    Object.keys(data.results[0]).forEach(function(key) {
-        const th = document.createElement('th');
-        th.textContent = key;
-        headerRow.appendChild(th);
+function createLineChart(elementId, label, labels, data) {
+    const ctx = document.getElementById(elementId).getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: label,
+                data: data,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                tension: 0.1
+            }]
+        }
     });
-    thead.appendChild(headerRow);
-    table.appendChild(thead);
-
-    // Add data rows
-    const tbody = document.createElement('tbody');
-    data.results.forEach(function(result) {
-        const row = document.createElement('tr');
-        Object.values(result).forEach(function(value) {
-            const td = document.createElement('td');
-            td.textContent = value;
-            row.appendChild(td);
-        });
-        tbody.appendChild(row);
-    });
-    table.appendChild(tbody);
-
-    // Add the table to the element
-    element.appendChild(table);
 }
 
-// Call the APIs and display the data in tables
+function createBarChart(elementId, label, labels, data) {
+    const ctx = document.getElementById(elementId).getContext('2d');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: label,
+                data: data,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+// 문제 유형별 정확도
 fetchWithToken('/api/performance/question-type')
     .then(response => response.json())
-    .then(data => displayDataInTable('accuracy-by-type', data))
+    .then(data => {
+        const labels = data.results.map(result => `Q${result.question_id}`);
+        const accuracies = data.results.map(result => parseFloat(result.accuracy));
+        createLineChart('accuracy-by-type', 'Accuracy by Question Type', labels, accuracies);
+    })
     .catch(err => console.error(err));
 
+// 취약한 영역
 fetchWithToken('/api/performance/question-level')
     .then(response => response.json())
-    .then(data => displayDataInTable('weak-areas', data))
+    .then(data => {
+        const labels = data.results.map(result => `Level ${result.question_level}`);
+        const accuracies = data.results.map(result => parseFloat(result.accuracy));
+        createBarChart('weak-areas', 'Weak Areas', labels, accuracies);
+    })
     .catch(err => console.error(err));
 
-fetchWithToken('/api/performance/time-spent')
-    .then(response => response.json())
-    .then(data => displayDataInTable('accuracy-time-by-test', data))
-    .catch(err => console.error(err));
-
+// 테스트별 진행상황 및 성과
 fetchWithToken('/api/growth')
     .then(response => response.json())
-    .then(data => displayDataInTable('progress-by-test', data))
+    .then(data => {
+        const labels = data.results.map(result => new Date(result.created_at).toLocaleDateString());
+        const accuracies = data.results.map(result => parseFloat(result.accuracy));
+        createLineChart('progress-by-test', 'Progress and Performance by Test', labels, accuracies);
+    })
     .catch(err => console.error(err));
