634f4560747a9eb7d168c4f96c8eeb5bb95c90b2
ruo <ruo@gravylab.co.kr>
2023-06-27 09:40:06 +0900

    fix: rank js

--- static/js/rank.js
+++ static/js/rank.js
@@ -10,15 +10,21 @@ function fetchWithToken(url, options = {}) {
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    // 로그인된 사용자의 이름을 가져옵니다.
-    fetchWithToken('/user/status')
-        .then(response => response.json())
-        .then(status => {
-            if (status.status === 'logged_in') {
-                // 로그인된 사용자의 이름을 전역 변수로 저장합니다.
-                window.loggedInUsername = status.username;
-            }
-        });
+    const jwtToken = localStorage.getItem('access_token');
+    if (jwtToken) {
+        // 로그인된 사용자의 이름을 가져옵니다.
+        fetchWithToken('/user/status')
+            .then(response => response.json())
+            .then(status => {
+                if (status.status === 'logged_in') {
+                    // 로그인된 사용자의 이름을 전역 변수로 저장합니다.
+                    window.loggedInUsername = status.username;
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
 
     // 문제 유형을 가져와서 선택박스에 추가합니다.
     fetch('/api/question_types')
@@ -59,7 +65,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     const tr = document.createElement('tr');
 
                     // 로그인된 사용자의 행이면 색상을 변경합니다.
-                    if (window.loggedInUsername === userRanking.username) {
+                    if (jwtToken && userRanking.username === window.loggedInUsername) {
                         tr.className = 'highlight';
                     }
 
