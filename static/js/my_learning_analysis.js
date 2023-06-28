d0b07cb2771040bc6844a91e2322f6db4f17f56b
ruo <ruo@gravylab.co.kr>
2023-06-28 12:45:00 +0900

    fix: my_learning_analysis js

--- static/js/my_learning_analysis.js
+++ static/js/my_learning_analysis.js
@@ -95,7 +95,7 @@ function createStackedBarChart(elementId, labels, datasets, yAxisUnit, stepSize)
                             if (label) {
                                 label += ': ';
                             }
-                            label += context.parsed.y;
+                            label += context.parsed.y + ' ' + yAxisUnit;
                             return label;
                         }
                     }
@@ -119,6 +119,17 @@ function createStackedBarChart(elementId, labels, datasets, yAxisUnit, stepSize)
             }
         }
     });
+}
+
+// 색상을 변화시키는 함수
+function shadeColor(color, percent) {
+    const f = parseInt(color.slice(1), 16),
+        t = percent < 0 ? 0 : 255,
+        p = percent < 0 ? percent * -1 : percent,
+        R = f >> 16,
+        G = (f >> 8) & 0x00FF,
+        B = f & 0x0000FF;
+    return "#" + ((0x1000000 + (Math.round((t - R) * p) + R) * 0x10000 + (Math.round((t - G) * p) + G) * 0x100 + (Math.round((t - B) * p) + B)).toString(16).slice(1));
 }
 
 window.onload = function() {
