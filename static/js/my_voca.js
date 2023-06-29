884306c8ba3b8ef0005aa0bcb19d89e9ee1612d8
ruo <ruo@gravylab.co.kr>
2023-06-29 16:16:03 +0900

    fix: my_voca css

--- static/js/my_voca.js
+++ static/js/my_voca.js
@@ -152,12 +152,99 @@ function showQuestion(wordId) {
     // Add event listeners to the favourite and report buttons
     let favouriteBtn = questionDiv.querySelector(".favourite-btn");
     let reportBtn = questionDiv.querySelector(".report-btn");
-    favouriteBtn.addEventListener("click", function() {
-        console.log("Favourite button clicked for question " + this.dataset.questionId);
-        // TODO: Implement favourite feature
+
+    // Fetch favourite status and color the button accordingly
+    fetchWithToken('/api/get_favourite_status?question_id=' + question.QuestionId, {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'favourite') {
+            favouriteBtn.classList.add('fav');
+            favouriteBtn.style.color = '#ff4a4a';  // Change button color to red
+        }
     });
+
+    favouriteBtn.addEventListener("click", function() {
+        var question_id = this.getAttribute('data-question-id');
+
+        // Check if button is already marked as favourite
+        var isFavourite = this.classList.contains('fav');
+
+        if (isFavourite) {
+            // If the question is already in favourites, remove it
+            fetchWithToken('/api/favourite/question', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    question_id: question_id
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    alert('즐겨찾기에서 삭제하는 데 실패했습니다: ' + data.error);
+                } else {
+                    this.classList.remove('fav');
+                    this.style.color = 'gray';  // Change button color to gray
+                    alert('즐겨찾기에서 성공적으로 삭제되었습니다.');
+                }
+            });
+        } else {
+            // If the question is not in favourites, add it
+            fetchWithToken('/api/favourite/question', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    question_id: question_id
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    alert('즐겨찾기에 추가하는 데 실패했습니다: ' + data.error);
+                } else {
+                    this.classList.add('fav');
+                    this.style.color = '#ff4a4a';  // Change button color to red
+                    alert('즐겨찾기에 성공적으로 추가되었습니다.');
+                }
+            });
+        }
+    });
+
     reportBtn.addEventListener("click", function() {
-        console.log("Report button clicked for question " + this.dataset.questionId);
-        // TODO: Implement report feature
+        var question_id = this.getAttribute('data-question-id');
+        var report_content = prompt('리포트 내용을 입력하세요:');
+        if (report_content) {
+            fetchWithToken('/api/report/question', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    question_id: question_id,
+                    report_content: report_content,
+                    report_type: 'question'
+                })
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("API request failed: " + response.status);
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.error) {
+                    alert('리포트를 보내는 데 실패했습니다: ' + data.error);
+                } else {
+                    alert('리포트가 성공적으로 전송되었습니다.');
+                }
+            })
+            .catch(error => alert(error));
+        }
     });
 }
