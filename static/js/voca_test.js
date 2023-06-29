24e8e79abb0d0d152ed22a34095ee881bc4c9ee9
ruo <ruo@gravylab.co.kr>
2023-06-29 12:37:42 +0900

    fix: voca_test js

--- static/js/voca_test.js
+++ static/js/voca_test.js
@@ -30,3 +30,93 @@ function fetchWithToken(url, options = {}) {
     }
 }
 
+// 페이지 초기화
+let vocabularies = [];
+let currentVocabulary = 0;
+
+function loadVocabularies() {
+    fetchWithToken('/api/vocabularies')
+        .then(response => response.json())
+        .then(data => {
+            vocabularies = data.vocabularies;
+            displayVocabulary();
+        });
+}
+
+function displayVocabulary() {
+    let vocaContainer = document.getElementById('voca-test-container');
+    let vocabulary = vocabularies[currentVocabulary];
+
+    // 초기화
+    vocaContainer.innerHTML = '';
+
+    let wordElement = document.createElement('h2');
+    wordElement.textContent = vocabulary.word;
+    vocaContainer.appendChild(wordElement);
+
+    // 임의의 순서로 답안 배치
+    let answers = [...vocabulary.wrong_explanations, vocabulary.explanation];
+    answers.sort(() => Math.random() - 0.5);
+
+    answers.forEach(answer => {
+        let answerElement = document.createElement('button');
+        answerElement.textContent = answer;
+        answerElement.addEventListener('click', function() {
+            checkAnswer(answer);
+            this.disabled = true;
+        });
+        vocaContainer.appendChild(answerElement);
+    });
+
+    let passElement = document.createElement('button');
+    passElement.textContent = "모르겠음";
+    passElement.addEventListener('click', function() {
+        checkAnswer(null);
+        this.disabled = true;
+    });
+    vocaContainer.appendChild(passElement);
+
+    let nextElement = document.createElement('button');
+    nextElement.textContent = "다음 단어";
+    nextElement.addEventListener('click', function() {
+        nextVocabulary();
+    });
+    vocaContainer.appendChild(nextElement);
+}
+
+function checkAnswer(answer) {
+    let correct = (answer === vocabularies[currentVocabulary].explanation);
+    let messageElement = document.createElement('h3');
+    messageElement.textContent = correct ? "정답입니다" : "오답입니다";
+    document.getElementById('voca-test-container').appendChild(messageElement);
+
+    let answerElements = document.querySelectorAll('#voca-test-container button:not(:last-child)');
+    answerElements.forEach(answerElement => {
+        if (answerElement.textContent === vocabularies[currentVocabulary].explanation) {
+            answerElement.style.color = 'green';
+        }
+        answerElement.disabled = true;
+    });
+
+    fetchWithToken('/api/user_vocabularies', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            word_id: vocabularies[currentVocabulary].id,
+            wrong_count: correct ? 0 : 1
+        })
+    });
+}
+
+function nextVocabulary() {
+    if (currentVocabulary < vocabularies.length - 1) {
+        currentVocabulary += 1;
+        displayVocabulary();
+    } else {
+        loadVocabularies();
+    }
+}
+
+loadVocabularies();
