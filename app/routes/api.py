644e15c01a193dc0127f53b2969482d2b76f63e9
ruo <ruo@gravylab.co.kr>
2023-07-03 11:04:10 +0900

    fix: mynote js api

--- app/routes/api.py
+++ app/routes/api.py
@@ -348,12 +348,14 @@ def get_user_tests():
     username = get_jwt_identity()
     page = request.args.get('page', default=1, type=int)
 
-    tests = db.session.query(UserTestDetail)\
+    pagination = db.session.query(UserTestDetail)\
         .filter(UserTestDetail.username == username)\
         .order_by(desc(UserTestDetail.created_at))\
-        .paginate(page, per_page=6)
+        .paginate(page=page, per_page=6, error_out=False)
 
-    return jsonify({"tests": [serialize_user_test_detail(test) for test in tests.items]}), 200
+    tests = pagination.items
+
+    return jsonify({"tests": [serialize_user_test_detail(test) for test in tests]}), 200
 
 
 @api.route('/my-note/tests/<int:test_id>/wrong-questions', methods=['GET'])
