ebc2f0339b983f8f153007fa784b4d726e23e8d2
ruo <ruo@gravylab.co.kr>
2023-07-09 15:34:35 +0900

    fix: mynote css

--- static/js/my_note.js
+++ static/js/my_note.js
@@ -203,20 +203,26 @@ let modal = document.getElementById('myModal');
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.animationName = "slidedown";
+    modal.addEventListener('animationend', function() {
+        modal.style.display = "none";
+        modal.style.animationName = "slideup";  // Reset the animation
+    }, {once: true});
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.animationName = "slidedown";
+        modal.addEventListener('animationend', function() {
+            modal.style.display = "none";
+            modal.style.animationName = "slideup";  // Reset the animation
+        }, {once: true});
+    }
 }
+
 
 function loadWrongQuestions(testId, testNo) {
-
     fetchWithToken('/api/my-note/tests/' + testId + '/wrong-questions')
     .then(response => response.json())
     .then(data => {
