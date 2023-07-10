8efb1cf056faaff5200c0f226ec8361f944770ff
ruo <ruo@gravylab.co.kr>
2023-07-10 09:20:23 +0900

    fix: mynote js css html

--- static/js/my_note.js
+++ static/js/my_note.js
@@ -59,6 +59,12 @@ function timeSince(date) {
 }
 
 document.querySelector('.collapsible').addEventListener('click', function() {
+    const upIcon = this.querySelector('.fa-chevron-up');
+    const downIcon = this.querySelector('.fa-chevron-down');
+
+    upIcon.style.transform = `rotate(${parseInt(getComputedStyle(upIcon).transform.split(',')[1]) + 180}deg)`;
+    downIcon.style.transform = `rotate(${parseInt(getComputedStyle(downIcon).transform.split(',')[1]) + 180}deg)`;
+
     this.classList.toggle('collapsed');
 });
 
