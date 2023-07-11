11d33fb1ef847e3db6197234a0b79f02cbfd95b5
ruo <ruo@gravylab.co.kr>
2023-07-11 18:53:44 +0900

    fix: layout js

--- static/js/layout.js
+++ static/js/layout.js
@@ -126,6 +126,7 @@ function scrollFunction() {
     let title = document.getElementById("mobile-title");
     let titleLink = document.querySelector("#mobile-title a")
     let mobileUserIcon = document.getElementById("mobile-user-icon");
+    let loginArea = document.getElementById("login-area");
 
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         navbar.style.height = "10px";
@@ -133,14 +134,28 @@ function scrollFunction() {
         title.style.fontSize = "10px";
         title.style.fontWeight = "500";
         titleLink.style.color = '#bdb9b9'; // 폰트 색상을 다크 그레이로 변경
-        mobileUserIcon.style.scale = "0.5";
+        if (loginArea) {
+            mobileUserIcon.style.color = '#bdb9b9';
+            loginArea.style.scale = '0.5';
+            loginArea.style.border = '1px solid #bdb9b9';
+        }
+        else {
+            mobileUserIcon.style.scale = "0.5";
+        }
     } else {
         navbar.style.height = "50px";
         navbar.style.backgroundColor = '#f87272';
         title.style.fontSize = "1.5rem";
         title.style.fontWeight = "700";
         titleLink.style.color = '#fff'; // 폰트 색상을 원래대로(흰색) 변경
-        mobileUserIcon.style.scale = "1";
+        if (loginArea) {
+            mobileUserIcon.style.color = '#fff';
+            loginArea.style.scale = '1';
+            loginArea.style.border = '1px solid #fff';
+        }
+        else {
+            mobileUserIcon.style.scale = "1";
+        }
     }
 }
 
